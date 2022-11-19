03ca9cc71206f54be776b93cfaecb4109c988502
nithin gudala <nithin@esoftconnect.com>
2022-11-19 18:44:17 -0500

    updated new code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './block/app-layout/layout/layout.component';
+import {HeaderComponent} from './block/header/header/header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        component: LayoutComponent
+      },
+      {
+        path: 'home',
+        component: HeaderComponent
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './block/footer/footer/footer.component';
+import { HeaderComponent } from './block/header/header/header.component';
+import { NavigationComponent } from './block/navigation/navigation/navigation.component';
+import { LayoutComponent } from './block/app-layout/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
